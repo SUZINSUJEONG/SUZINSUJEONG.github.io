7dafe21af0341315861589847e25486c73d88827
[SUZINSUJEONG] <[rnlduadl177@gmail.com]>
2023-05-10 19:02:30 +0900

    동두천 삭제

--- site/cng/js/common.js
+++ site/cng/js/common.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     });
     // #top_btn
     $(window).scroll(function () {
-      if ($(this).scrollTop() > 100) {
+      if ($(this).scrollTop() > 80) {
         $('#top_btn').fadeIn('slow');
       } else {
         $('#top_btn').fadeOut('slow');
@@ -14,7 +14,7 @@ $(document).ready(function(){
     });
 
     $(window).scroll(function () {
-      if ($(this).scrollTop() > 100) {
+      if ($(this).scrollTop() > 80) {
         $('header').addClass('on');
       } else {
         $('header').removeClass('on');
@@ -76,6 +76,48 @@ $(document).ready(function(){
     $(this).removeClass("on");
     $(".menu_close").fadeOut();
     $(".btn_menu_btn").removeClass("on");
+
+  });
+
+  //tab
+  $('#tabs-nav li:first-child').addClass('active');
+  $('.tab-content').hide();
+  $('.tab-content:first').show();
+
+  // Click function
+  $('#tabs-nav li').click(function(){
+    $('#tabs-nav li').removeClass('active');
+    $(this).addClass('active');
+    $('.tab-content').hide();
+    
+    var activeTab = $(this).find('a').attr('href');
+    $(activeTab).fadeIn();
+    return false;
+  });
+
+
+  //sub2_1
+  // function onScroll() {
+  //   if ($(window).scrollTop() > $(".product").height()) {
+  //     $('.product_bar').addClass('on');
+  //     // $('.main').css({"position": "inherit", "top": "auto"});
+  //   } else {
+  //     $('.product_bar').removeClass('on');
+  //    // $('.main').css({"position": "sticky", "top": "0"});
+
+  //   }
+  // }
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > $(".product").height() ) {
+      $('.product_bar').addClass('on');
+      $('.product_bar').css({"position": "fixed","width": "310px"});
+
+    } else {
+      $('.product_bar').removeClass('on');
+      $('.product_bar').css({"position": "inherit","width": "auto"});
+
+    }
 
   });
 
